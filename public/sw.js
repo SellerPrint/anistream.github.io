36d2b08fb9a634c2dd0692de46e4a0728c7f7ca7
SellerPrint <tretisgame999@gmail.com>
2026-03-29 15:32:57 +0000

    Update service worker to include secondary config
    Merged two service workers and added secondary configuration.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,22 @@
+// Service Worker fusionné - sw en tête puis sw2
 self.options = {
     "domain": "3nbf4.com",
     "zoneId": 10803100
 }
 self.lary = ""
+
+// Import du premier service worker (sw)
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+
+// Configuration secondaire (sw2)
+self.options2 = {
+    "domain": "5gvci.com",
+    "zoneId": 10803118
+}
+
+// Import du second service worker (sw2)
+try {
+    importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+} catch(e) {
+    console.log('Second SW import:', e)
+}
